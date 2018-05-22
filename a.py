e66289176c3078dd8f90f5c1487437dd9b18225c
armseke03 <36103728+armseke03@users.noreply.github.com>
2018-05-23 00:42:33 +0700

    Create a.py

---
+++ a.py
@@ -0,0 +1,328 @@
+# -*- coding: utf-8 -*-
+
+from LineAPI.linepy import *
+from LineAPI.akad.ttypes import Message
+from LineAPI.akad.ttypes import ContentType as Type
+from gtts import gTTS
+from time import sleep
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from googletrans import Translator
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, six, ast, pytz, urllib, urllib3, urllib.parse, traceback, atexit, subprocess
+
+ririn = LINE("EtAMLgb0zoJwV5h2ESr6.7kqscP17dKQEF08Bg5AKnG.LkvtwSoDccEW4SQQb9rQh1/2dUXRv11BQfcebyDgolg='''''")
+#ririn = LINE("TOKENMU")
+
+ririnMid = ririn.profile.mid
+ririnProfile = ririn.getProfile()
+ririnSettings = ririn.getSettings()
+ririnPoll = OEPoll(ririn)
+botStart = time.time()
+
+print ("╔═════════════════════════\n║╔════════════════════════\n║╠❂➣ DNA BERHASIL LOGIN\n║╚════════════════════════\n╚═════════════════════════")
+
+msg_dict = {}
+
+wait = {
+    "autoAdd": True,
+    "autoJoin": True,
+    "autoLeave": False,
+    "autoRead": False,
+    "autoRespon": True,
+    "autoResponPc": False,
+    "autoJoinTicket": True,
+    "checkContact": False,
+    "checkPost": False,
+    "checkSticker": False,
+    "changePictureProfile": False,
+    "changeGroupPicture": [],
+    "keyCommand": "",
+    "leaveRoom": True,
+    "myProfile": {
+        "displayName": "",
+        "coverId": "",
+        "pictureStatus": "",
+        "statusMessage": ""
+    },
+    "mimic": {
+        "copy": False,
+        "status": False,
+        "target": {}
+    },
+    "Protectcancel": True,
+    "Protectgr": True,
+    "Protectinvite": True,
+    "Protectjoin": False,
+    "setKey": False,
+    "sider": False,
+    "unsendMessage": True
+}
+
+cctv = {
+    "cyduk":{},
+    "point":{},
+    "sidermem":{}
+}
+
+read = {
+    "ROM": {},
+    "readPoint": {},
+    "readMember": {},
+    "readTime": {}
+}
+
+list_language = {
+    "list_textToSpeech": {
+        "id": "Indonesia",
+        "af" : "Afrikaans",
+        "sq" : "Albanian",
+        "ar" : "Arabic",
+        "hy" : "Armenian",
+        "bn" : "Bengali",
+        "ca" : "Catalan",
+        "zh" : "Chinese",
+        "zh-cn" : "Chinese (Mandarin/China)",
+        "zh-tw" : "Chinese (Mandarin/Taiwan)",
+        "zh-yue" : "Chinese (Cantonese)",
+        "hr" : "Croatian",
+        "cs" : "Czech",
+        "da" : "Danish",
+        "nl" : "Dutch",
+        "en" : "English",
+        "en-au" : "English (Australia)",
+        "en-uk" : "English (United Kingdom)",
+        "en-us" : "English (United States)",
+        "eo" : "Esperanto",
+        "fi" : "Finnish",
+        "fr" : "French",
+        "de" : "German",
+        "el" : "Greek",
+        "hi" : "Hindi",
+        "hu" : "Hungarian",
+        "is" : "Icelandic",
+        "id" : "Indonesian",
+        "it" : "Italian",
+        "ja" : "Japanese",
+        "km" : "Khmer (Cambodian)",
+        "ko" : "Korean",
+        "la" : "Latin",
+        "lv" : "Latvian",
+        "mk" : "Macedonian",
+        "no" : "Norwegian",
+        "pl" : "Polish",
+        "pt" : "Portuguese",
+        "ro" : "Romanian",
+        "ru" : "Russian",
+        "sr" : "Serbian",
+        "si" : "Sinhala",
+        "sk" : "Slovak",
+        "es" : "Spanish",
+        "es-es" : "Spanish (Spain)",
+        "es-us" : "Spanish (United States)",
+        "sw" : "Swahili",
+        "sv" : "Swedish",
+        "ta" : "Tamil",
+        "th" : "Thai",
+        "tr" : "Turkish",
+        "uk" : "Ukrainian",
+        "vi" : "Vietnamese",
+        "cy" : "Welsh"
+    },
+    "list_translate": {    
+        "af": "afrikaans",
+        "sq": "albanian",
+        "am": "amharic",
+        "ar": "arabic",
+        "hy": "armenian",
+        "az": "azerbaijani",
+        "eu": "basque",
+        "be": "belarusian",
+        "bn": "bengali",
+        "bs": "bosnian",
+        "bg": "bulgarian",
+        "ca": "catalan",
+        "ceb": "cebuano",
+        "ny": "chichewa",
+        "zh-cn": "chinese (simplified)",
+        "zh-tw": "chinese (traditional)",
+        "co": "corsican",
+        "hr": "croatian",
+        "cs": "czech",
+        "da": "danish",
+        "nl": "dutch",
+        "en": "english",
+        "eo": "esperanto",
+        "et": "estonian",
+        "tl": "filipino",
+        "fi": "finnish",
+        "fr": "french",
+        "fy": "frisian",
+        "gl": "galician",
+        "ka": "georgian",
+        "de": "german",
+        "el": "greek",
+        "gu": "gujarati",
+        "ht": "haitian creole",
+        "ha": "hausa",
+        "haw": "hawaiian",
+        "iw": "hebrew",
+        "hi": "hindi",
+        "hmn": "hmong",
+        "hu": "hungarian",
+        "is": "icelandic",
+        "ig": "igbo",
+        "id": "indonesian",
+        "ga": "irish",
+        "it": "italian",
+        "ja": "japanese",
+        "jw": "javanese",
+        "kn": "kannada",
+        "kk": "kazakh",
+        "km": "khmer",
+        "ko": "korean",
+        "ku": "kurdish (kurmanji)",
+        "ky": "kyrgyz",
+        "lo": "lao",
+        "la": "latin",
+        "lv": "latvian",
+        "lt": "lithuanian",
+        "lb": "luxembourgish",
+        "mk": "macedonian",
+        "mg": "malagasy",
+        "ms": "malay",
+        "ml": "malayalam",
+        "mt": "maltese",
+        "mi": "maori",
+        "mr": "marathi",
+        "mn": "mongolian",
+        "my": "myanmar (burmese)",
+        "ne": "nepali",
+        "no": "norwegian",
+        "ps": "pashto",
+        "fa": "persian",
+        "pl": "polish",
+        "pt": "portuguese",
+        "pa": "punjabi",
+        "ro": "romanian",
+        "ru": "russian",
+        "sm": "samoan",
+        "gd": "scots gaelic",
+        "sr": "serbian",
+        "st": "sesotho",
+        "sn": "shona",
+        "sd": "sindhi",
+        "si": "sinhala",
+        "sk": "slovak",
+        "sl": "slovenian",
+        "so": "somali",
+        "es": "spanish",
+        "su": "sundanese",
+        "sw": "swahili",
+        "sv": "swedish",
+        "tg": "tajik",
+        "ta": "tamil",
+        "te": "telugu",
+        "th": "thai",
+        "tr": "turkish",
+        "uk": "ukrainian",
+        "ur": "urdu",
+        "uz": "uzbek",
+        "vi": "vietnamese",
+        "cy": "welsh",
+        "xh": "xhosa",
+        "yi": "yiddish",
+        "yo": "yoruba",
+        "zu": "zulu",
+        "fil": "Filipino",
+        "he": "Hebrew"
+    }
+}
+
+try:
+    with open("Log_data.json","r",encoding="utf_8_sig") as f:
+        msg_dict = json.loads(f.read())
+except:
+    print("Couldn't read Log data")
+    
+wait["myProfile"]["displayName"] = ririnProfile.displayName
+wait["myProfile"]["statusMessage"] = ririnProfile.statusMessage
+wait["myProfile"]["pictureStatus"] = ririnProfile.pictureStatus
+coverId = ririn.getProfileDetail()["result"]["objectId"]
+wait["myProfile"]["coverId"] = coverId
+
+def restartBot():
+    print ("[ INFO ] BOT RESTART")
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+    
+def logError(text):
+    ririn.log("[ ERROR ] {}".format(str(text)))
+    tz = pytz.timezone("Asia/Jakarta")
+    timeNow = datetime.now(tz=tz)
+    timeHours = datetime.strftime(timeNow,"(%H:%M)")
+    day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+    hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
+    bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    inihari = datetime.now(tz=tz)
+    hr = inihari.strftime('%A')
+    bln = inihari.strftime('%m')
+    for i in range(len(day)):
+        if hr == day[i]: hasil = hari[i]
+    for k in range(0, len(bulan)):
+        if bln == str(k): bln = bulan[k-1]
+    time = "{}, {} - {} - {} | {}".format(str(hasil), str(inihari.strftime('%d')), str(bln), str(inihari.strftime('%Y')), str(inihari.strftime('%H:%M:%S')))
+    with open("logError.txt","a") as error:
+        error.write("\n[ {} ] {}".format(str(time), text))
+
+def cTime_to_datetime(unixtime):
+    return datetime.fromtimestamp(int(str(unixtime)[:len(str(unixtime))-3]))
+def dt_to_str(dt):
+    return dt.strftime('%H:%M:%S')
+
+def delete_log():
+    ndt = datetime.now()
+    for data in msg_dict:
+        if (datetime.utcnow() - cTime_to_datetime(msg_dict[data]["createdTime"])) > timedelta(1):
+            if "path" in msg_dict[data]:
+                ririn.deleteFile(msg_dict[data]["path"])
+            del msg_dict[data]
+            
+def sendMention(to, text="", mids=[]):
+    arrData = ""
+    arr = []
+    mention = "@dee "
+    if mids == []:
+        raise Exception("Invalid mids")
+    if "@!" in text:
+        if text.count("@!") != len(mids):
+            raise Exception("Invalid mids")
+        texts = text.split("@!")
+        textx = ""
+        for mid in mids:
+            textx += str(texts[mids.index(mid)])
+            slen = len(textx)
+            elen = len(textx) + 15
+            arrData = {'S':str(slen), 'E':str(elen - 4), 'M':mid}
+            arr.append(arrData)
+            textx += mention
+        textx += str(texts[len(mids)])
+    else:
+        textx = ""
+        slen = len(textx)
+        elen = len(textx) + 15
+        arrData = {'S':str(slen), 'E':str(elen - 4), 'M':mids[0]}
+        arr.append(arrData)
+        textx += mention + str(text)
+    ririn.sendMessage(to, textx, {'MENTION': str('{"MENTIONEES":' + json.dumps(arr) + '}')}, 0)
+
+def command(text):
+    pesan = text.lower()
+    if wait["setKey"] == True:
+        if pesan.startswith(wait["keyCommand"]):
+            cmd = pesan.replace(wait["keyCommand"],"")
+        else:
+            cmd = "Undefined command"
+    else:
+        cmd = text.lower()
+    return cmd
